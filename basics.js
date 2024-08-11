028219e2e002439d374e1efb2daee4929d2b77e3
abhinav3593 <163002463+abhinav3593@users.noreply.github.com>
2024-08-11 14:34:23 +0530

    Add files via upload

---
+++ basics.js
@@ -0,0 +1,18 @@
+// react and nextjs are frameoworks
+//browser only runs html js and css and WebAssembly
+//line by line execution compile at runtime
+//runnning is faster when compilation is done before running
+//js does this one thing together with the running so it runs
+/// bit slower in comparison
+// to the cpp
+//pronetoruntimeerrors
+// CPP type casts the bool to a number
+//slow becz of garbage colector
+  
+var a=1;
+a = "hammm";
+a= true;
+
+
+
+
